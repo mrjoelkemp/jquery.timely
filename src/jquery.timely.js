4e7de605df57a4794fbdcb350cc940dcf06d3d3b
Joel Kemp <mrjoelkemp@gmail.com>
2014-01-03 14:04:48 -0500

    Monthly grid rendering in color with dummy intensity data

--- src/jquery.timely.js
+++ src/jquery.timely.js
@@ -9,61 +9,111 @@
 
   // Options:
   //    period: 'monthly', 'daily' (TODO)
+  //    data: object used to determine color strength
   $.fn.timely = function (options) {
     options = $.extend({ period: 'monthly' }, options);
 
-    var $this = $(this),
-        $template;
+    var
+        $this = $(this),
+        // Grab the element before it's in the dom to minimize potential reflow
+        $grid = getPeriodGrid(options.period);
 
-    if (options.period.toLowerCase() === 'monthly') {
-      $template = $(periodTemplate);
+    // renderPeriod.call(this, options.period);
 
-      // Fill out the grid
-      $template.append(getMonthlyGrid());
+    options.data = generateDebugData();
 
-      // Inject the template into the target
-      $this.append($template);
-    }
+    renderColors($grid, options.data);
+
+    $this.html($grid);
   };
 
-  // Returns a dom template for the monthly period
-  var getMonthlyGrid = function () {
-    var $day = $(unitTemplate),
-    //     monthsWith31days = [1, 3, 5, 7, 8, 10, 12],
-        $template = $(gridTemplate),
-        $week, absoluteDay;
-    //     month, day, numDays;
+  var
+      // Returns a jQuery element representing the generated dom for
+      // the passed period
+      getPeriodGrid = function (period) {
+        var $template;
 
-    // for (month = 1; month <= 12; month++) {
-    //   numDays = monthsWith31days.indexOf(month) !== -1 ? 31 : 30;
+        if (period.toLowerCase() === 'monthly') {
+          $template = $(periodTemplate);
 
-    //   for (day = 1; day <= numDays; day++) {
+          // Fill out the grid
+          $template.append(getMonthlyGrid());
+        }
 
-    //   }
-    // }
+        return $template;
+      },
 
-    for (var week = 1; week <= 52; week++) {
-      $week = $('<div class="week"></div>');
-      $week.addClass(' ' + week);
-      $week.data('value', week);
+      // Keys are day numbers and values are floats between 0 (no activity) and 1 (max activity)
+      // TODO: keys as timestamps?
+      renderColors = function ($grid, data) {
+        var units = $grid.find('.time-unit'),
+            unitValueIndexMap = {};
 
-      for (var day = 1; day <= 7; day++) {
-        $day = $(unitTemplate);
-        // Day 1 to 365
-        absoluteDay = day + (7 * (week - 1));
+        // Generate a lookup table of unit values to dom element positions in units list
+        $.each(units, function (idx, val) {
+          var unitVal = $(val).data('value');
+          console.log('unitval', unitVal);
 
-        $day.addClass('day' + ' ' + absoluteDay);
+          if (typeof unitVal !== 'undefined') {
+            unitValueIndexMap[unitVal] = idx;
+          }
+        });
 
-        $day.data('value', day);
+        $.each(data, function (unitValue, intensity) {
+          var element = units[unitValueIndexMap[unitValue]];
 
-        $week.append($day);
-      }
+          // Color intensity for the unit
+          // TODO: Make color channel configurable (select red, green or blue)
+          // OR just use the intensity to dictate the alpha map and allow a base color as an option
+          if (element) element.style.background = 'rgb(0,' + Math.round(255 * intensity) + ',0)';
+        });
+      };
 
-      $template.append($week);
+  // Debug helper
+  var generateDebugData = function () {
+    var data = {};
 
+    // For every day of the year
+    for (var i = 1; i < 365; i++) {
+      data[i] = Math.random().toFixed(2);
     }
 
-    return $template;
+    return data;
   };
+
+  /////////////////////////
+  // Time period helpers
+  /////////////////////////
+
+  var
+      // Returns a dom representation for the months of a year
+      getMonthlyGrid = function () {
+        var $day = $(unitTemplate),
+            $template = $(gridTemplate),
+            $week, absoluteDay;
+
+        for (var week = 1; week <= 52; week++) {
+          $week = $('<div class="week"></div>');
+          $week
+            .addClass(week.toString())
+            .data('value', week);
+
+          for (var day = 1; day <= 7; day++) {
+            // Day 1 to 364
+            absoluteDay = day + (7 * (week - 1));
+
+            $day = $(unitTemplate);
+            $day
+              .addClass('day' + ' ' + absoluteDay)
+              .data('value', absoluteDay);
+
+            $week.append($day);
+          }
+
+          $template.append($week);
+        }
+
+        return $template;
+      };
 
 })(window.jQuery);
